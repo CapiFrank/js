c57fae22a449d294a6a1db8cffc25836bc7cd711
CapiFrank <59805803+CapiFrank@users.noreply.github.com>
2022-06-28 01:00:52 -0600

    Se modifica el grosor del borde

--- tablescript.js
+++ tablescript.js
@@ -47,14 +47,14 @@ function get_personas(URL) {
 
                         var inner_celda = document.createElement("td");
                         var textoCelda = document.createTextNode(usuario.tel_tr);
-                        inner_celda.style.borderLeft = "black 2px solid";
+                        inner_celda.style.borderLeft = "black 1,5px solid";
                         inner_celda.appendChild(textoCelda);
                         inner_hilera.appendChild(inner_celda);
                         inner_tabla.appendChild(inner_hilera);
 
                         var inner_celda = document.createElement("td");
                         var textoCelda = document.createTextNode(usuario.tel_otr);
-                        inner_celda.style.borderLeft = "black 2px solid";
+                        inner_celda.style.borderLeft = "black 1,5px solid";
                         inner_celda.appendChild(textoCelda);
                         inner_hilera.appendChild(inner_celda);
                         inner_tabla.appendChild(inner_hilera);
@@ -77,14 +77,14 @@ function get_personas(URL) {
 
                         var inner_celda = document.createElement("td");
                         var textoCelda = document.createTextNode(usuario.cor_tr);
-                        inner_celda.style.borderLeft = "black 2px solid";
+                        inner_celda.style.borderLeft = "black 1,5px solid";
                         inner_celda.appendChild(textoCelda);
                         inner_hilera.appendChild(inner_celda);
                         inner_tabla.appendChild(inner_hilera);
 
                         var inner_celda = document.createElement("td");
                         var textoCelda = document.createTextNode(usuario.cor_otr);
-                        inner_celda.style.borderLeft = "black 2px solid";
+                        inner_celda.style.borderLeft = "black 1,5px solid";
                         inner_celda.appendChild(textoCelda);
                         inner_hilera.appendChild(inner_celda);
                         inner_tabla.appendChild(inner_hilera);
