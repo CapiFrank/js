1dda8b3bfa07f58506e38c6397f409db5eea612f
CapiFrank <59805803+CapiFrank@users.noreply.github.com>
2022-07-05 12:29:24 -0600

    Se cambia la forma de presentar los telefonos y direcciones

--- tablescript.js
+++ tablescript.js
@@ -35,64 +35,52 @@ function get_personas(URL) {
                         tblBody.appendChild(hilera);
 
                         //------------------------------------------------------------
+                                
                         var celda = document.createElement("td");
-                        var inner_tabla = document.createElement("table");
-                        var inner_hilera = document.createElement("tr");
-
-                        var inner_celda = document.createElement("td");
                         var textoCelda = document.createTextNode(usuario.tel_per);
-                        inner_celda.appendChild(textoCelda);
-                        inner_hilera.appendChild(inner_celda);
-                        inner_tabla.appendChild(inner_hilera);
+                        celda.appendChild(textoCelda);
+                        hilera.appendChild(celda);        
+                        tblBody.appendChild(hilera);
 
-                        var inner_celda = document.createElement("td");
+                        //------------------------------------------------------------
+                                
+                        var celda = document.createElement("td");
                         var textoCelda = document.createTextNode(usuario.tel_tr);
-                        inner_celda.appendChild(textoCelda);
-                        inner_celda.style.borderLeft = "black 1.5px solid";
-                        inner_hilera.appendChild(inner_celda);
-                        inner_tabla.appendChild(inner_hilera);
+                        celda.appendChild(textoCelda);
+                        hilera.appendChild(celda);        
+                        tblBody.appendChild(hilera);
+                                
+                        //------------------------------------------------------------
 
-                        var inner_celda = document.createElement("td");
+                        var celda = document.createElement("td");
                         var textoCelda = document.createTextNode(usuario.tel_otr);
-                        inner_celda.appendChild(textoCelda);
-                        inner_celda.style.borderLeft = "black 1.5px solid";
-                        inner_hilera.appendChild(inner_celda);
-                        inner_tabla.appendChild(inner_hilera);
-
-                        celda.appendChild(inner_tabla);
-                        hilera.appendChild(celda);
+                        celda.appendChild(textoCelda);
+                        hilera.appendChild(celda);        
                         tblBody.appendChild(hilera);
 
                         //------------------------------------------------------------
 
                         var celda = document.createElement("td");
-                        var inner_tabla = document.createElement("table");
-                        var inner_hilera = document.createElement("tr");
-
-                        var inner_celda = document.createElement("td");
                         var textoCelda = document.createTextNode(usuario.cor_per);
-                        inner_celda.appendChild(textoCelda);
-                        inner_hilera.appendChild(inner_celda);
-                        inner_tabla.appendChild(inner_hilera);
-
-                        var inner_celda = document.createElement("td");
-                        var textoCelda = document.createTextNode(usuario.cor_tr);
-                        inner_celda.appendChild(textoCelda);
-                        inner_celda.style.borderLeft = "black 1.5px solid";
-                        inner_hilera.appendChild(inner_celda);
-                        inner_tabla.appendChild(inner_hilera);
-
-                        var inner_celda = document.createElement("td");
-                        var textoCelda = document.createTextNode(usuario.cor_otr);
-                        inner_celda.appendChild(textoCelda);
-                        inner_celda.style.borderLeft = "black 1.5px solid";
-                        inner_hilera.appendChild(inner_celda);
-                        inner_tabla.appendChild(inner_hilera);
-
-                        celda.appendChild(inner_tabla);
-                        hilera.appendChild(celda);
+                        celda.appendChild(textoCelda);
+                        hilera.appendChild(celda);        
                         tblBody.appendChild(hilera);
 
+                        //------------------------------------------------------------
+                                
+                        var celda = document.createElement("td");
+                        var textoCelda = document.createTextNode(usuario.cor_tr);
+                        celda.appendChild(textoCelda);
+                        hilera.appendChild(celda);        
+                        tblBody.appendChild(hilera);
+                                
+                        //------------------------------------------------------------
+
+                        var celda = document.createElement("td");
+                        var textoCelda = document.createTextNode(usuario.cor_otr);
+                        celda.appendChild(textoCelda);
+                        hilera.appendChild(celda);        
+                        tblBody.appendChild(hilera);
                         //------------------------------------------------------------
 
                         var celda = document.createElement("td");
