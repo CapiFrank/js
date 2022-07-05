dad98a04d26b0d10411a8339e7c56616d7bbfb09
CapiFrank <59805803+CapiFrank@users.noreply.github.com>
2022-07-05 14:27:16 -0600

    Se agregan las funciones para zona y habitat

--- tablescript.js
+++ tablescript.js
@@ -184,3 +184,127 @@ function get_guia(URL) {
                 console.error(error);
             });
         }
+function get_zona(URL) {
+            axios.get(URL).then((response) => {
+                const data = response.data;
+                const keys = Object.keys(data);
+                if (keys.length) {
+                    keys.map((key) => {
+                        const usuario = data[key];
+                        console.log(usuario)
+
+                        //------------------------------------------------------------
+
+                        var tblBody = document.getElementsByTagName('tbody')[0];
+                        var hilera = document.createElement("tr");
+                        var celda = document.createElement("td");
+                        var textoCelda = document.createTextNode(usuario.id);
+                        celda.appendChild(textoCelda);
+                        hilera.appendChild(celda);
+                        tblBody.appendChild(hilera);
+
+                        //------------------------------------------------------------
+
+                        var celda = document.createElement("td");
+                        var textoCelda = document.createTextNode(usuario.nombre);
+                        celda.appendChild(textoCelda);
+                        hilera.appendChild(celda);
+                        tblBody.appendChild(hilera);
+
+                        //------------------------------------------------------------
+
+                        var celda = document.createElement("td");
+                        var textoCelda = document.createTextNode(usuario.ext_ocup);
+                        celda.appendChild(textoCelda);
+                        hilera.appendChild(celda);
+                        tblBody.appendChild(hilera);
+
+                        //------------------------------------------------------------
+
+                        var celda = document.createElement("td");
+                        celda.innerHTML = `<a href="${URL}/eliminar/${usuario.id}">Eliminar</a>`;
+                        hilera.appendChild(celda);
+                        tblBody.appendChild(hilera);
+
+                        //------------------------------------------------------------
+
+                        var celda = document.createElement("td");
+                        celda.innerHTML = `<a href="${URL}/editar/${usuario.id}">Editar</a>`;
+                        hilera.appendChild(celda);
+                        tblBody.appendChild(hilera);
+                    });
+                }
+            }).catch((error) => {
+                console.error(error);
+            });
+        }
+function get_habitat(URL) {
+            axios.get(URL).then((response) => {
+                const data = response.data;
+                const keys = Object.keys(data);
+                if (keys.length) {
+                    keys.map((key) => {
+                        const usuario = data[key];
+                        console.log(usuario)
+
+                        //------------------------------------------------------------
+
+                        var tblBody = document.getElementsByTagName('tbody')[0];
+                        var hilera = document.createElement("tr");
+                        var celda = document.createElement("td");
+                        var textoCelda = document.createTextNode(usuario.id);
+                        celda.appendChild(textoCelda);
+                        hilera.appendChild(celda);
+                        tblBody.appendChild(hilera);
+
+                        //------------------------------------------------------------
+
+                        var celda = document.createElement("td");
+                        var textoCelda = document.createTextNode(usuario.nombre);
+                        celda.appendChild(textoCelda);
+                        hilera.appendChild(celda);
+                        tblBody.appendChild(hilera);
+
+                        //------------------------------------------------------------
+
+                        var celda = document.createElement("td");
+                        var textoCelda = document.createTextNode(usuario.clima);
+                        celda.appendChild(textoCelda);
+                        hilera.appendChild(celda);
+                        tblBody.appendChild(hilera);
+
+                        //------------------------------------------------------------
+
+                        var celda = document.createElement("td");
+                        var textoCelda = document.createTextNode(usuario.tipo_vegetacion);
+                        celda.appendChild(textoCelda);
+                        hilera.appendChild(celda);
+                        tblBody.appendChild(hilera);
+
+                        //------------------------------------------------------------
+
+                        var celda = document.createElement("td");
+                        var textoCelda = document.createTextNode(usuario.continente);
+                        celda.appendChild(textoCelda);
+                        hilera.appendChild(celda);
+                        tblBody.appendChild(hilera);
+
+                        //------------------------------------------------------------
+
+                        var celda = document.createElement("td");
+                        celda.innerHTML = `<a href="${URL}/eliminar/${usuario.id}">Eliminar</a>`;
+                        hilera.appendChild(celda);
+                        tblBody.appendChild(hilera);
+
+                        //------------------------------------------------------------
+
+                        var celda = document.createElement("td");
+                        celda.innerHTML = `<a href="${URL}/editar/${usuario.id}">Editar</a>`;
+                        hilera.appendChild(celda);
+                        tblBody.appendChild(hilera);
+                    });
+                }
+            }).catch((error) => {
+                console.error(error);
+            });
+        }
