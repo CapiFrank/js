4642b3686924ef9089a9fac91ee88b84bd1c0f09
CapiFrank <59805803+CapiFrank@users.noreply.github.com>
2022-07-05 14:21:15 -0600

    Se agregan las funciones para cuidador y guia

--- tablescript.js
+++ tablescript.js
@@ -60,3 +60,127 @@ function get_personas(URL) {
                 console.error(error);
             });
         }
+function get_cuidador(URL) {
+            axios.get(URL).then((response) => {
+                const data = response.data;
+                const keys = Object.keys(data);
+
+                if (keys.length) {
+                    keys.map((key) => {
+                        const usuario = data[key];
+                        console.log(usuario)
+
+                        //------------------------------------------------------------
+
+                        var tblBody = document.getElementsByTagName('tbody')[0];
+                        var hilera = document.createElement("tr");
+                        var celda = document.createElement("td");
+                        var textoCelda = document.createTextNode(usuario.cedula);
+                        celda.appendChild(textoCelda);
+                        hilera.appendChild(celda);
+                        tblBody.appendChild(hilera);
+
+                        //------------------------------------------------------------
+
+                        var celda = document.createElement("td");
+                        var textoCelda = document.createTextNode(usuario.nombre);
+                        celda.appendChild(textoCelda);
+                        hilera.appendChild(celda);
+                        tblBody.appendChild(hilera);
+
+                        //------------------------------------------------------------
+
+                        var celda = document.createElement("td");
+                        var textoCelda = document.createTextNode(usuario.fech_ingr);
+                        celda.appendChild(textoCelda);
+                        hilera.appendChild(celda);
+                        tblBody.appendChild(hilera);
+
+                        //------------------------------------------------------------
+
+                        var celda = document.createElement("td");
+                        celda.innerHTML = `<a href="${URL}/detalles/${usuario.cedula}">Detalles</a>`;
+                        hilera.appendChild(celda);
+                        tblBody.appendChild(hilera);
+                        
+                        //------------------------------------------------------------
+
+                        var celda = document.createElement("td");
+                        celda.innerHTML = `<a href="${URL}/editar/${usuario.cedula}">Editar</a>`;
+                        hilera.appendChild(celda);
+                        tblBody.appendChild(hilera);
+                                
+                        //------------------------------------------------------------
+
+                        var celda = document.createElement("td");
+                        celda.innerHTML = `<a href="${URL}/eliminar/${usuario.cedula}">Eliminar</a>`;
+                        hilera.appendChild(celda);
+                        tblBody.appendChild(hilera);
+                    });
+                }
+            }).catch((error) => {
+                console.error(error);
+            });
+        }
+function get_guia(URL) {
+            axios.get(URL).then((response) => {
+                const data = response.data;
+                const keys = Object.keys(data);
+
+                if (keys.length) {
+                    keys.map((key) => {
+                        const usuario = data[key];
+                        console.log(usuario)
+
+                        //------------------------------------------------------------
+
+                        var tblBody = document.getElementsByTagName('tbody')[0];
+                        var hilera = document.createElement("tr");
+                        var celda = document.createElement("td");
+                        var textoCelda = document.createTextNode(usuario.cedula);
+                        celda.appendChild(textoCelda);
+                        hilera.appendChild(celda);
+                        tblBody.appendChild(hilera);
+
+                        //------------------------------------------------------------
+
+                        var celda = document.createElement("td");
+                        var textoCelda = document.createTextNode(usuario.nombre);
+                        celda.appendChild(textoCelda);
+                        hilera.appendChild(celda);
+                        tblBody.appendChild(hilera);
+
+                        //------------------------------------------------------------
+
+                        var celda = document.createElement("td");
+                        var textoCelda = document.createTextNode(usuario.fecha_ini);
+                        celda.appendChild(textoCelda);
+                        hilera.appendChild(celda);
+                        tblBody.appendChild(hilera);
+
+                        //------------------------------------------------------------
+
+                        var celda = document.createElement("td");
+                        celda.innerHTML = `<a href="${URL}/detalles/${usuario.cedula}">Detalles</a>`;
+                        hilera.appendChild(celda);
+                        tblBody.appendChild(hilera);
+                        
+                        //------------------------------------------------------------
+
+                        var celda = document.createElement("td");
+                        celda.innerHTML = `<a href="${URL}/editar/${usuario.cedula}">Editar</a>`;
+                        hilera.appendChild(celda);
+                        tblBody.appendChild(hilera);
+                                
+                        //------------------------------------------------------------
+
+                        var celda = document.createElement("td");
+                        celda.innerHTML = `<a href="${URL}/eliminar/${usuario.cedula}">Eliminar</a>`;
+                        hilera.appendChild(celda);
+                        tblBody.appendChild(hilera);
+                    });
+                }
+            }).catch((error) => {
+                console.error(error);
+            });
+        }
