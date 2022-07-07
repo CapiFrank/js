37612e636add0d63b0a397c6431a322a80146513
CapiFrank <59805803+CapiFrank@users.noreply.github.com>
2022-07-06 20:53:35 -0600

    Se agregan los metodos de itinerario y especie

--- tablescript.js
+++ tablescript.js
@@ -308,3 +308,136 @@ function get_habitat(URL) {
                 console.error(error);
             });
         }
+function get_especie(URL) {
+            axios.get(URL).then((response) => {
+                const data = response.data;
+                const keys = Object.keys(data);
+                if (keys.length) {
+                    keys.map((key) => {
+                        const usuario = data[key];
+                        console.log(usuario)
+
+                        //------------------------------------------------------------
+
+                        var tblBody = document.getElementsByTagName('tbody')[0];
+                        var hilera = document.createElement("tr");
+                        var celda = document.createElement("td");
+                        var textoCelda = document.createTextNode(usuario.id);
+                        celda.appendChild(textoCelda);
+                        hilera.appendChild(celda);
+                        tblBody.appendChild(hilera);
+
+                        //------------------------------------------------------------
+
+                        var celda = document.createElement("td");
+                        var textoCelda = document.createTextNode(usuario.nombre);
+                        celda.appendChild(textoCelda);
+                        hilera.appendChild(celda);
+                        tblBody.appendChild(hilera);
+
+                        //------------------------------------------------------------
+
+                        var celda = document.createElement("td");
+                        var textoCelda = document.createTextNode(usuario.nombre_cientifico);
+                        celda.appendChild(textoCelda);
+                        hilera.appendChild(celda);
+                        tblBody.appendChild(hilera);
+
+                        //------------------------------------------------------------
+
+                        var celda = document.createElement("td");
+                        var textoCelda = document.createTextNode(usuario.descripcion);
+                        celda.appendChild(textoCelda);
+                        hilera.appendChild(celda);
+                        tblBody.appendChild(hilera);
+
+                        //------------------------------------------------------------
+
+                        var celda = document.createElement("td");
+                        celda.innerHTML = `<a href="${URL}/eliminar/${usuario.id}">Eliminar</a>`;
+                        hilera.appendChild(celda);
+                        tblBody.appendChild(hilera);
+
+                        //------------------------------------------------------------
+
+                        var celda = document.createElement("td");
+                        celda.innerHTML = `<a href="${URL}/editar/${usuario.id}">Editar</a>`;
+                        hilera.appendChild(celda);
+                        tblBody.appendChild(hilera);
+                    });
+                }
+            }).catch((error) => {
+                console.error(error);
+            });
+        }
+
+function get_itinerario(URL) {
+            axios.get(URL).then((response) => {
+                const data = response.data;
+                const keys = Object.keys(data);
+                if (keys.length) {
+                    keys.map((key) => {
+                        const usuario = data[key];
+                        console.log(usuario)
+
+                        //------------------------------------------------------------
+
+                        var tblBody = document.getElementsByTagName('tbody')[0];
+                        var hilera = document.createElement("tr");
+                        var celda = document.createElement("td");
+                        var textoCelda = document.createTextNode(usuario.codigo_itinerario);
+                        celda.appendChild(textoCelda);
+                        hilera.appendChild(celda);
+                        tblBody.appendChild(hilera);
+
+                        //------------------------------------------------------------
+
+                        var celda = document.createElement("td");
+                        var textoCelda = document.createTextNode(usuario.duracion_recorrido);
+                        celda.appendChild(textoCelda);
+                        hilera.appendChild(celda);
+                        tblBody.appendChild(hilera);
+
+                        //------------------------------------------------------------
+
+                        var celda = document.createElement("td");
+                        var textoCelda = document.createTextNode(usuario.longitud_recorrido);
+                        celda.appendChild(textoCelda);
+                        hilera.appendChild(celda);
+                        tblBody.appendChild(hilera);
+
+                        //------------------------------------------------------------
+
+                        var celda = document.createElement("td");
+                        var textoCelda = document.createTextNode(usuario.maximo_visitantes_Aut);
+                        celda.appendChild(textoCelda);
+                        hilera.appendChild(celda);
+                        tblBody.appendChild(hilera);
+
+                        //------------------------------------------------------------
+
+                        var celda = document.createElement("td");
+                        var textoCelda = document.createTextNode(usuario.especie);
+                        celda.appendChild(textoCelda);
+                        hilera.appendChild(celda);
+                        tblBody.appendChild(hilera);
+
+                        //------------------------------------------------------------
+
+                        var celda = document.createElement("td");
+                        celda.innerHTML = `<a href="${URL}/eliminar/${usuario.id}">Eliminar</a>`;
+                        hilera.appendChild(celda);
+                        tblBody.appendChild(hilera);
+
+                        //------------------------------------------------------------
+
+                        var celda = document.createElement("td");
+                        celda.innerHTML = `<a href="${URL}/editar/${usuario.id}">Editar</a>`;
+                        hilera.appendChild(celda);
+                        tblBody.appendChild(hilera);
+                    });
+                }
+            }).catch((error) => {
+                console.error(error);
+            });
+        }
